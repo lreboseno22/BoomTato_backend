f07554c56723df88e0747221f10b2c3d09790b00
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-10-16 15:53:02 -0400

    get all games AND get game by id

--- routes/games.mjs
+++ routes/games.mjs
@@ -35,4 +35,25 @@ router.post("/", async (req, res) => {
     }
 });
 
+// Get/Read Game
+router.get("/", async (req, res) => {
+    try {
+        const games = await Game.find();
+        res.json(games);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Get/Read Game by id
+router.get("/:id", async (req, res) => {
+    try {
+        const game = await Game.findById(req.params.id);
+        if(!game) return res.status(404).json({ message: "Game not Found" });
+        res.json(game);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
+
 export default router;
