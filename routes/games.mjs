26f97e94589bbdfff261fef71ad21372faff424d
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-10-16 15:26:48 -0400

    check if host already has an active game

--- routes/games.mjs
+++ routes/games.mjs
@@ -7,9 +7,28 @@ const router = express.Router();
 router.post("/", async (req, res) => {
     try {
         const { name, host } = req.body;
-        const newGame = await Game.create({ name, host, players: [host] });
-        const savedGame = await newGame.save();
 
+        // Check if player/host already has a game open
+        const existingGame = await Game.findOne({
+            host,
+            status: { $in: ["waiting", "in progress"] } // only if the game's status is in progress or waiting
+        });
+
+        // If the player does have a game that isn't finihsed then throw error
+        if(existingGame){
+            return res.status(400).json({
+                message: "You already have an active game. Finish it or cancle it first!"
+            })
+        }
+
+        const newGame = new Game({ 
+            name, 
+            host, 
+            players: [host],
+            status: "waiting"
+        });
+
+        const savedGame = await newGame.save();
         res.status(201).json(savedGame);
     } catch (err) {
         res.status(500).json({ message: err.message });
